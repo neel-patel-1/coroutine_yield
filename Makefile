a997dd87db1d0b36754c9b7aacc63e0f5dcbf126
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-06-03 10:18:07 -0500

    cpu exe time

--- Makefile
+++ Makefile
@@ -5,13 +5,16 @@ CXX_FLAGS=-I/home/n869p538/spr-accel-profiling/interrupt_qat_dc/DML/install/incl
 LDD_FLAGS=-lboost_coroutine -ldml -lqat -lusdm -L/home/n869p538/spr-accel-profiling/interrupt_qat_dc/DML/install/lib/ -L/home/n869p538/spr-accel-profiling/interrupt_qat_dc/qatlib/.libs
 FLAGS= -DBREAKDOWN
 
-all: baseline-ctx-switch single-request-latency accel_notify_context_switch
+all: baseline-ctx-switch single-request-latency accel_notify_context_switch baseline-cpu
 
 accel_notify_context_switch: main.cpp
 	$(CXX) -o accel_notify_context_switch main.cpp $(LDD_FLAGS)  $(CXX_FLAGS) $(FLAGS)
 
 baseline-ctx-switch: baseline-ctx-switch.cpp
 	$(CXX) -o baseline-ctx-switch baseline-ctx-switch.cpp $(LDD_FLAGS)  $(CXX_FLAGS) $(FLAGS)
+
+baseline-cpu: baseline-cpu.cpp
+	$(CXX) -o baseline-cpu baseline-cpu.cpp $(LDD_FLAGS)  $(CXX_FLAGS) $(FLAGS)
 
 single-request-latency: single-request-latency.cpp
 	$(CXX) -o single-request-latency single-request-latency.cpp $(LDD_FLAGS)  $(CXX_FLAGS) $(FLAGS)
---
+++ baseline-cpu.cpp
@@ -0,0 +1,219 @@
+#include <boost/coroutine/all.hpp>
+
+#include <cstdlib>
+#include <iostream>
+#include <immintrin.h>
+#include <vector>
+
+#include <x86intrin.h>
+#include <thread>
+#include <pthread.h>
+#include <sched.h>
+
+#include <boost/bind.hpp>
+
+#include "dml/dml.hpp"
+#include "dml/dml.h"
+
+typedef boost::coroutines::symmetric_coroutine< void >  coro_t;
+
+/* Coroutine for Request 2 */
+
+/* Test Params */
+static constexpr int num_requests = 1000;
+
+uint8_t * source;
+uint8_t * destination;
+
+/* C API Batch Test Params */
+#define BUFFER_SIZE  4096 // 2 MB
+#define PADDING_SIZE 4096 // DML_OP_DUALCAST requirement "dst1 and dst2 address bits 11:0 must be the same"
+#define BATCH_COUNT  5u   // 7 ops for this batch operation
+#define PATTERN_SIZE 8u   // pattern size is always 8
+
+/* Wait Functions */
+static __always_inline void umonitor(const volatile void *addr)
+{
+	asm volatile(".byte 0xf3, 0x48, 0x0f, 0xae, 0xf0" : : "a"(addr));
+}
+
+static __always_inline int umwait(unsigned long timeout, unsigned int state)
+{
+	uint8_t r;
+	uint32_t timeout_low = (uint32_t)timeout;
+	uint32_t timeout_high = (uint32_t)(timeout >> 32);
+
+	asm volatile(".byte 0xf2, 0x48, 0x0f, 0xae, 0xf1\t\n"
+		"setc %0\t\n"
+		: "=r"(r)
+		: "c"(state), "a"(timeout_low), "d"(timeout_high));
+	return r;
+}
+
+
+/* Stats Utils */
+template <typename NUMTYPE>
+static inline void gen_diff_array(NUMTYPE dst_array, NUMTYPE array1, NUMTYPE array2, int size)
+{
+  for(int i=0; i<size; i++){ dst_array[i] = array2[i] - array1[i]; }
+}
+
+#define do_sum_array(accum,array,iter) accum = 0; \
+ for (int i=1; i<iter; i++){ accum+=array[i]; } \
+ accum /= iter
+#define do_avg(sum, itr) (sum/itr)
+
+#define avg_samples_from_arrays(yield_to_submit, avg, before_yield, before_submit, num_samples) \
+  gen_diff_array(yield_to_submit, before_submit, before_yield, num_samples); \
+  do_sum_array(avg, yield_to_submit, num_samples); \
+  do_avg(avg, num_samples);
+
+#ifdef BREAKDOWN
+static constexpr int num_samples = num_requests;
+int cur_sample = 0;
+uint64_t before_submit[num_samples];
+uint64_t before_yield[num_samples];
+uint64_t after_yield[num_samples];
+uint64_t after_resume[num_samples];
+uint64_t request_start_time[num_samples];
+uint64_t before_resume[num_samples];
+uint64_t after_complete[num_samples];
+#endif
+
+#ifdef BREAKDOWN
+uint64_t before_job_alloc[num_samples];
+uint64_t after_job_alloc[num_samples];
+
+uint64_t before_job_prepare[num_samples];
+uint64_t after_job_prepare[num_samples];
+
+uint64_t after_job_complete[num_samples];
+#endif
+
+
+int cur_request = 0;
+dml::handler<dml::batch_operation, std::allocator<std::uint8_t>> handlers[num_requests]; /* I want to have multiple requests in flight and have requests being preempted */
+dml_job_t *dml_job_ptr;
+int next_response_idx = 0;
+
+coro_t::call_type *c1 = 0;
+
+
+
+void request_fn( coro_t::yield_type &yield){
+  dml_status_t status;
+  uint32_t job_size_ptr;
+  #ifdef BREAKDOWN
+  request_start_time[cur_sample] = __rdtsc();
+  #endif
+
+  source = (uint8_t *)malloc(BUFFER_SIZE);;
+  destination = (uint8_t *)malloc(BUFFER_SIZE);;
+
+  for(int i = 0; i < BUFFER_SIZE; i++){
+      source[i] = i % 256;
+  }
+  uint32_t crc = 1;
+  int next_submit_idx = cur_request;
+
+
+
+  #ifdef BREAKDOWN
+  before_job_alloc[cur_sample] = __rdtsc();
+  #endif
+
+  status = dml_get_job_size(DML_PATH_SW, &job_size_ptr);
+  if(status != DML_STATUS_OK){
+    std::cerr << "Job size determination failed\n";
+  }
+
+  dml_job_ptr = (dml_job_t *) malloc(job_size_ptr);
+
+  #ifdef BREAKDOWN
+  after_job_alloc[cur_sample] = __rdtsc();
+  #endif
+
+  #ifdef BREAKDOWN
+  before_job_prepare[cur_sample] = __rdtsc();
+  #endif
+
+  status = dml_init_job(DML_PATH_SW, dml_job_ptr);
+  if(status != DML_STATUS_OK){
+    std::cerr << "Job initialization failed\n";
+  }
+  dml_job_ptr->operation = DML_OP_COPY_CRC;
+  dml_job_ptr->source_first_ptr       = source;
+  dml_job_ptr->destination_first_ptr  = destination;
+  dml_job_ptr->source_length          = BUFFER_SIZE;
+  dml_job_ptr->crc_checksum_ptr       = &crc;
+
+  #ifdef BREAKDOWN
+  after_job_prepare[cur_sample] = __rdtsc();
+  #endif
+
+
+
+  #ifdef BREAKDOWN
+  before_submit[cur_sample] = __rdtsc();
+  #endif
+
+  status = dml_execute_job(dml_job_ptr, DML_WAIT_MODE_BUSY_POLL);
+  if (DML_STATUS_OK != status) {
+      printf("An error (%u) occured during job execution .\n", status);
+      dml_finalize_job(dml_job_ptr);
+      free(dml_job_ptr);
+      exit(-1);
+  }
+  #ifdef BREAKDOWN
+  after_job_complete[cur_sample] = __rdtsc();
+  #endif
+
+
+  cur_request++;
+
+
+  for(int i = 0; i < BUFFER_SIZE; i++){
+    if(destination[i] != source[i]){
+        printf("Error: Operation result is incorrect.\n");
+        dml_finalize_job(dml_job_ptr);
+        free(dml_job_ptr);
+        exit(-1);
+    }
+  }
+  status = dml_finalize_job(dml_job_ptr);
+  if (DML_STATUS_OK != status) {
+      printf("An error (%u) occured during job finalization.\n", status);
+      free(dml_job_ptr);
+      exit(-1);
+  }
+  free(dml_job_ptr);
+  free(source);
+  free(destination);
+}
+
+
+int main( int argc, char * argv[])
+{
+  int core = 5;
+
+
+
+  for(int i=0; i<num_samples; i++){
+    coro_t::call_type coro1(request_fn);
+    c1 = &coro1;
+    coro1();
+    cur_sample++;
+  }
+
+  #ifdef BREAKDOWN
+  uint64_t avg = 0;
+  uint64_t yield_to_submit[num_samples];
+
+
+  avg_samples_from_arrays(yield_to_submit, avg, after_job_complete, request_start_time,num_samples);
+  std::cout << "CPU-Exe-Time: " << avg << std::endl;
+
+  #endif
+
+    return EXIT_SUCCESS;
+}
